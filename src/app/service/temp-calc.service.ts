60f991d781acd2e1a76a5d7fdd0797cc74e898b5
UnkJester <UnkJester@users.noreply.github.com>
2020-05-09 11:37:20 -0500

    final number fix

--- src/app/service/temp-calc.service.ts
+++ src/app/service/temp-calc.service.ts
@@ -7,26 +7,26 @@ export class TempCalcService {
   constructor() { }
 
   public convertFToC(fahrenheit: number): number {
-    return ((Number(fahrenheit) - 32) * 5) / 9;
+    return ((fahrenheit as number) - 32) * 5 / 9;
   }
 
   public convertFToK(fahrenheit: number): number{
-    return this.convertCToK(this.convertFToC(Number(fahrenheit)));
+    return this.convertCToK(this.convertFToC(fahrenheit as number));
   }
 
   public convertCToF(celsius: number): number {
-    return Number(celsius) * 1.8000 + 32;
+    return (celsius as number) * 1.8000 + 32;
   }
 
   public convertCToK(celsius: number): number {
-    return Number(celsius) + 273.15;
+    return (celsius as number) + 273.15;
   }
 
   public convertKToF(kelvin: number): number {
-    return this.convertCToF(this.convertKToC(Number(kelvin)));
+    return this.convertCToF(this.convertKToC(kelvin as number));
   }
 
   public convertKToC(kelvin: number): number {
-    return Number(kelvin) - 273.15;
+    return (kelvin as number) - 273.15;
   }
 }
